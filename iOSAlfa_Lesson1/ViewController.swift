1f10aef4991d345b03ef0202a53d1d7e2ca50070
Faculdade Alfa <faculdadealfa@mac-15.local>
2019-02-09 08:33:50 -0200

    Initial Commit

---
+++ iOSAlfa_Lesson1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSAlfa_Lesson1
+//
+//  Created by Faculdade Alfa on 09/02/2019.
+//  Copyright © 2019 Faculdade Alfa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
